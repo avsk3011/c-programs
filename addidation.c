587156cb72c4ea6c04934639b946cbeeb8723cbd
avsk3011 <avsk3011@gmail.com>
2022-12-14 22:45:05 +0530

    addidation.c

---
+++ addidation.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,sum;
+print("\n Enter the Value to A:");
+scanf("%d", &a);
+printf("\n Enter value to B:");
+scanf(%d",&b);
+sub=a+b;
+printf("\n sub=%d",sub);
+return 0;
+}
